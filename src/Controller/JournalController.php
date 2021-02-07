fdbe349bb25a6001d089f4c265d2d6c63d200ae0
jeremytrips <51384000+jeremytrips@users.noreply.github.com>
2021-02-07 16:55:50 +0100

    Adding views to retrieve List and Task.
    +tasklist.html template used to show each task of a list.
    o index.html Adding for loop to display each task lists.
    o JournalController modifying response context

--- src/Controller/JournalController.php
+++ src/Controller/JournalController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\TaskList;
+use App\Repository\TaskListRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
+
 
 class JournalController extends AbstractController
 {
     /**
      * @Route("/", name="journal")
      */
-    public function index(): Response
+    public function index(TaskListRepository $repo): Response
     {
-        return $this->render('journal/index.html.twig', [
-            
+        // Index of the website. Retrieve every lists and return them in the context.
+        return $this->render(
+            'journal/index.html.twig', [
+                'tasklists' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="task_list")
+     */
+    public function FunctionName(TaskList $taskList, LoggerInterface $logger)
+    {
+        // Return the id list and display all it's task.
+        // Use paramConverter to return the list with id = id   
+        return $this->render(
+            'journal/tasklist.html.twig', [
+                'task_list' => $taskList,
         ]);
     }
 }
