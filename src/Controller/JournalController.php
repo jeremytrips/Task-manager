9b8b6c1ee5611d02d85d9891cdd6941054a790a3
jeremytrips <51384000+jeremytrips@users.noreply.github.com>
2021-03-05 11:12:08 +0100

    Update on journal controller
    + Added journal description in the TaskList creation form
    + Added D TaskList functionality at "/delete/{id}"
    + Added C List form in the ListTaskOfAList view
    + Added C list functionality at "/task/create/{id}" where id is tasklist id
    + Added U functionality at "/Task/modify/{id}" where id is tasklist id
    + Added set reset functionality at "task/set/{id}" where id is the task
    + Added D task functionality at "task/delete/{id}" where id is the task
    o Modify the requirements of the arguement on ListTaskOfAList
    o Update html for better ux
    o Update html to handle post and forms

--- src/Controller/JournalController.php
+++ src/Controller/JournalController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use DateTime;
 use DateTimeImmutable;
 use Psr\Log\LoggerInterface;
 
 use App\Entity\TaskList;
 use App\Repository\TaskListRepository;
-
+use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,18 +27,25 @@ class JournalController extends AbstractController
     /**
      * @Route("/", name="lists")
      */
-    public function index(TaskListRepository $repo): Response
+    public function index(TaskListRepository $repo, LoggerInterface $logger)
     {
+        // Return the id Tasklist and display all tasks.
+     
         // Index of the website. Retrieve every lists and return them in the context.
-        $defaultData = ['message'=>'Description'];
+        $defaultData = ['message'=>'dunno'];
         $form = $this->createFormBuilder($defaultData)
                      ->setAction($this->generateUrl('new_task_list'))
                      ->add('title',
-                         TextType::class,[
+                            TextType::class,[
                             'label' => 'New list:',
                             'label_attr' => ['class' => "col-form-label"],
                             'attr' => ['placeholder' => "List title", 'class' => "form-control"],
                             'constraints' => [new NotBlank()]
+                        ])
+                        ->add('description',
+                            TextType::class, [
+                            'label'=> false,
+                            'attr' => ['placeholder' => "Description", 'class' => "form-control"],
                         ])
                      ->getForm();
        
@@ -53,11 +61,11 @@ class JournalController extends AbstractController
      */
     public function CreateNewList(Request $request, EntityManagerInterface $manager, LoggerInterface $logger)
     {
-        // Check if the ParameterBag has a size.
-
+        // Return the id Tasklist and display all tasks.
         $taskList = new TaskList();
         $form = $this->createFormBuilder($taskList)
                      ->add("title")
+                     ->add("description")
                      ->getForm();
 
         $form->handleRequest($request);
@@ -68,7 +76,7 @@ class JournalController extends AbstractController
             $manager->flush();
             return $this->redirectToRoute("task_list", ['id'=>$taskList->getId()]);
         }
-
+        // Todo redirect with errors
         return $this->render(
             'test.html.twig'
         );
@@ -77,23 +85,111 @@ class JournalController extends AbstractController
     /**
      * @Route("/delete/{id}", name="delete_list")
      */
-    public function DeleteList(TaskList $taskList, EntityManagerInterface $manager)
+    public function DeleteList($id, TaskListRepository $repo, EntityManagerInterface $manager, LoggerInterface $logger)
     {
+        $taskList = $repo->find($id);
         $manager->remove($taskList);
         $manager->flush();
-
-        return $this->redirectToRoute("task_list");
+        // todo maybe add flash message
+        return $this->redirectToRoute("lists");
     }
 
     /**
-     * @Route("/{id}", name="task_list")
+     * @Route("/{id}", name="task_list", requirements={"id":"\d+"})
      */
-    public function ListTask(TaskList $taskList)
+    public function ListTaskOfAList(TaskList $taskList, LoggerInterface $logger)
     {
-        // Return the id Tasklist and display all tasks.
+        $defaultData = ['message'=>'Description'];
+        $form = $this->createFormBuilder($defaultData)
+                     ->setAction($this->generateUrl('create_task', ['id' => $taskList->getId()]))
+                     ->add('description',
+                         TextType::class,[
+                            'label' => 'New list:',
+                            'label_attr' => ['class' => "col-form-label"],
+                            'attr' => ['placeholder' => "List title", 'class' => "form-control"],
+                            'constraints' => [new NotBlank()]
+                        ])
+                     ->getForm();
+        
         return $this->render(
             'journal/tasklist.html.twig', [
                 'task_list' => $taskList,
+                'task_form' => $form->createView()
         ]);
     }
+    
+    /**
+     * @Route("/task/create/{id}", name="create_task")
+     */
+    public function NewTask($id, TaskListRepository $repo, Request $request, EntityManagerInterface $manager, LoggerInterface $logger)
+    {
+        $task = new Task();
+        $form = $this->createFormBuilder($task)
+                     ->add("description")
+                     ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $taskList = $repo->find($id);
+            $taskList->setDateModified(new DateTime());
+            $manager->flush();
+            $task->setDateCreated(new DateTimeImmutable())
+                     ->setIsDone(false)
+                     ->setTaskList($taskList);
+            $manager->persist($task);
+            $manager->flush();
+            return $this->redirectToRoute("task_list", ['id'=>$id]);
+        }
+        // Todo redirect with errors
+        return $this->render(
+            'test.html.twig'
+        );
+    }
+
+    /**
+     * @Route("task/modify/{id}", name="modify_task")
+     */
+    public function ModifyTask($id, Request $request, EntityManagerInterface $manager, TaskRepository $repo)
+    {
+        $task = $repo->find($id);
+        $listId = $task->getTaskList()->getId();
+        $form = $this->createForm(Task::class, $task);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->flush();
+            return $this->redirectToRoute("task_list", ['id'=>$listId]);
+        }
+
+        // Todo redirect with errors
+        return $this->render(
+            'test.html.twig',
+        );
+    }
+
+    /**
+     * @Route("task/set/{id}", name="set_reset_task")
+     */
+    public function SetResetTask($id, EntityManagerInterface $manager, TaskRepository $repo )
+    {
+        $task = $repo->find($id);
+        $listId = $task->getTaskList()->getId(); 
+        $task->setIsDone(!$task->getIsDone());
+        $manager->flush();
+        return $this->redirectToRoute("task_list", ['id'=>$listId]);
+    }
+
+    /**
+     * @Route("task/delete/{id}", name="delete_task")
+     */
+    public function DeleteTask(Task $task, EntityManagerInterface $manager)
+    {
+        $taskList = $task->getTaskList();
+        $taskList->setDateModified(new DateTime());
+        $manager->flush();
+        $manager->remove($task);
+        $manager->flush();
+        return $this->redirectToRoute("task_list", ['id'=>$taskList->getId()]);
+    }
+
 }
